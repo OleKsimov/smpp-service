7b21f4b46a2067fdd77470303bd684324f417587
O.Movchan <oleksii.movchan@otpbank.ua>
2020-10-06 11:18:55 +0300

    project setup

--- src/main/java/ua/otpbank/ExampleResource.java
+++ src/main/java/ua/otpbank/ExampleResource.java
@@ -1,16 +1,20 @@
 package ua.otpbank;
 
-import javax.ws.rs.GET;
+import javax.ws.rs.Consumes;
+import javax.ws.rs.POST;
 import javax.ws.rs.Path;
 import javax.ws.rs.Produces;
-import javax.ws.rs.core.MediaType;
+import javax.ws.rs.core.Response;
 
-@Path("/hello")
+import static javax.ws.rs.core.MediaType.APPLICATION_JSON;
+
+@Path("/")
+@Consumes(APPLICATION_JSON)
+@Produces(APPLICATION_JSON)
 public class ExampleResource {
 
-    @GET
-    @Produces(MediaType.TEXT_PLAIN)
-    public String hello() {
-        return "hello";
+    @POST
+    public Response sendMessage() {
+        return null;
     }
 }
